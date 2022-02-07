4d846ecb8038898142f2e0d1c54bec3702ecd9e0
Łukasz Łęcki <lukasz.lecki@pomagier.info>
2022-02-07 23:59:59 +0100

    update to newest

---
+++ UNIVERSAL_JOINT_v3_2/panelize.bat
@@ -0,0 +1,8 @@
+kikit panelize \
+    UNIVERSAL_JOINT_v3_1.kicad_pcb UNIVERSAL_JOINT_v3_1_panel.kicad_pcb
+
+
+    --tabs 'fixed; width: 3mm' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
+    --post 'millradius: 1mm' \
+        --layout 'grid; rows: 2; cols: 2; space: 2mm' \
